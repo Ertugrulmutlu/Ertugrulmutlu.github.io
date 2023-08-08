d245eba2e5e59d9b5e7a7678c4f8504aa400fe1a
Ertugrulmutlu <65208049+Ertugrulmutlu@users.noreply.github.com>
2023-08-08 12:25:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 // Function to scroll to a specific section
 function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
   }
-  function updateProgressBar() {
-    const progressBar = document.getElementById('progressbarr');
-    const progress = (currentIndex / sectionIds.length) * 100;
-    progressBar.style.width = `${progress}%`;
+}
+
+function updateProgressBar() {
+  const progressBar = document.getElementById('progressbarr');
+  const progress = (currentIndex / sectionIds.length) * 100;
+  progressBar.style.width = `${progress}%`;
+
+  if (progress >= 100) {
+    const progressBar_display = document.getElementById('display_progresbar');
+    progressBar_display.style.display = 'none'; // Hide the progress bar
+    const somethingElse = document.getElementById('elevator');
+    somethingElse.style.display = 'block'; // Display something else
   }
-  // Scroll through sections every 4 seconds
-  const sectionIds = ['1', '2', '3'];
-  let currentIndex = 0;
-  
-  function scrollThroughSections() {
+}
+
+
+const sectionIds = ['0', '1', '2', '3', '4','5'];
+let currentIndex = 0;
+
+function scrollThroughSections() {
+  if (currentIndex < sectionIds.length) {
+    scrollToSection(sectionIds[currentIndex]);
+    currentIndex++;
+    updateProgressBar();
+
     if (currentIndex < sectionIds.length) {
-      scrollToSection(sectionIds[currentIndex]);
-      currentIndex++;
-      updateProgressBar()
-  
-      if (currentIndex < sectionIds.length) {
-        setTimeout(scrollThroughSections, 6000); // Adjust the delay to your preference
-      }
+      setTimeout(scrollThroughSections, 50); // Scroll every 4 seconds
     }
   }
-  
-  // Start scrolling
-  setTimeout(scrollThroughSections, 6000);
-  
-  // Update the progress bar based on the current index
+}
+
+// Start scrolling
+setTimeout(scrollThroughSections, 0);
